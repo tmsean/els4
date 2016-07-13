84189dc6e79ac04539793961f00a03b6d85aba04
Sean <minhson293@gmail.com>
2016-07-13 16:32:53 +0700

    remove space

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :active_relationships, class_name:  "Relationship",
+  has_many :active_relationships, class_name: "Relationship",
     foreign_key: "follower_id", dependent: :destroy
-  has_many :passive_relationships, class_name:  "Relationship",
+  has_many :passive_relationships, class_name: "Relationship",
     foreign_key: "followed_id", dependent: :destroy                              
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,4 @@
 class Word < ActiveRecord::Base
-	has_many :results
-
-	belongs_to :course
+  has_many :results
+  belongs_to :course
 end
